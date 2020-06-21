ae2c66f3dcbbcd93b8a20437e16cc9e16e87d9e4
Jimmy Smith <jasmith.188@gmail.com>
2020-06-21 01:11:50 -0400

    folder structure

---
+++ src/pages/Main.js
@@ -0,0 +1,13 @@
+import React from "react"
+
+function Main() {
+    return (
+       <div>
+           <h1>This is the Main Page...</h1>
+       </div>
+            
+        
+    )
+}
+
+export default Main
