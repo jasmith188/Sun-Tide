4610362362a8f5504fd65a8a3d5a25fe2a6aaf0e
Jimmy Smith <jasmith.188@gmail.com>
2020-06-23 16:57:12 -0400

    adding css to page

--- src/components/Weather/index.js
+++ src/components/Weather/index.js
@@ -6,7 +6,12 @@ class Weather extends React.Component {
         return (
            
             <div>
-                <h4>This is the Weather</h4>
+
+                { this.props.city && this.props.country && <p>Location: { this.props.city }, { this.props.country }</p> }
+                { this.props.temperature && <p>Temperature: { this.props.temperature }</p>}
+                { this.props.humidity && <p>Humidity: { this.props.humidity }%</p>}
+                { this.props.description && <p>Description: { this.props.description }</p>}
+                { this.props.error && <p>{ this.props.error } </p>}
             </div>
         )
     }
--- src/pages/Main.js
+++ src/pages/Main.js
@@ -6,21 +6,55 @@ import Form from "../components/Form";
 const API_KEY = "c5fc998f4951203abe90d5f6c1f39d7b";
 
 class Main extends React.Component {
+    state = {
+        temperature: undefined,
+        city: undefined,
+        country: undefined,
+        humidity: undefined,
+        description: undefined,
+        error: undefined
+    }
     getWeather = async (e) => {
         e.preventDefault();
         const city = e.target.elements.city.value;
         const country = e.target.elements.country.value;
-        const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}`);
+        const api_call = await fetch(`http://api.openweathermap.org/data/2.5/weather?q=${city},${country}&appid=${API_KEY}&units=imperial`);
         const data = await api_call.json();
-        console.log(data);
-        
+        if (city && country) {
+            console.log(data);
+            this.setState({
+                temperature: data.main.temp,
+                city: data.name,
+                country: data.sys.country,
+                humidity: data.main.humidity,
+                description: data.weather[0].description,
+                error: ""
+            });
+        } else {
+            this.setState({
+                temperature: undefined,
+                city: undefined,
+                country: undefined,
+                humidity: undefined,
+                description: undefined,
+                error: "Please type a value..."
+            });
+        }
     }
     render() {
         return (
             <div>
-                <Weather />
+                <Form getWeather={this.getWeather} />
+                <Weather
+                    temperature={this.state.temperature}
+                    city={this.state.city}
+                    country={this.state.country}
+                    humidity={this.state.humidity}
+                    description={this.state.description}
+                    error={this.state.error}
+                />
                 <Tide />
-                <Form getWeather={this.getWeather}/>
+                
             </div>
 
 
