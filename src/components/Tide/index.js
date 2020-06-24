98f5a856251d1f119fffe0e7f1c318d32ff9bcfe
Jimmy Smith <jasmith.188@gmail.com>
2020-06-24 18:13:25 -0400

    two apis working

--- src/components/Tide/index.js
+++ src/components/Tide/index.js
@@ -1,12 +1,34 @@
 import React from "react"
+import Card from 'react-bootstrap/Card'
+
+const API_KEY = "dad127b0-b3d6-11ea-a563-0242ac130002-dad12864-b3d6-11ea-a563-0242ac130002"
+
 
 class Tide extends React.Component {
+    state = {
+        lat: undefined,
+        lng: undefined
+    }
+
+    
+
+
+    componentDidMount() {
+        navigator.geolocation.getCurrentPosition(function (position) {
+            console.log("Latitude is :", position.coords.latitude);
+            console.log("Longitude is :", position.coords.longitude);
+            console.log(position);
+            
+
+        });
+    }
+
 
     render() {
         return (
 
             <div>
-                <h1>This is the Tide</h1>
+                
             </div>
         )
     }
